6186383952e56695eaeffa5bfce567989ff7a174
Edson Machado <edsonjmv@gmail.com>
2021-09-15 08:44:05 +0200

    format signin & signup components

--- packages/auth/src/components/Signin.js
+++ packages/auth/src/components/Signin.js
@@ -28,26 +28,26 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   '@global': {
     a: {
-      textDecoration: 'none',
-    },
+      textDecoration: 'none'
+    }
   },
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
     width: '100%',
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
 export default function SignIn({ onSignIn }) {
--- packages/auth/src/components/Signup.js
+++ packages/auth/src/components/Signup.js
@@ -25,26 +25,26 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   '@global': {
     a: {
-      textDecoration: 'none',
-    },
+      textDecoration: 'none'
+    }
   },
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
     width: '100%',
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(3)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
 export default function SignUp({ onSignIn }) {
